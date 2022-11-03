6a680b5ad608d6f594cf14017cc8b0d8a7440144
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2022-11-03 18:11:20 -0400

    crud blog view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from django.views.generic import View
+from django.views.generic import View, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .forms import PostCreateForm
 from .models import Post
 # Create your views here.
@@ -41,3 +42,18 @@ class BlogDetailView( View ):
     post = get_object_or_404( Post, pk=pk )
     context = { 'post': post }
     return render( request, 'blog-detail.html', context)
+
+class BlogUpdateView( UpdateView ):
+  
+  model  = Post
+  fields = ['title', 'content']
+  template_name = 'blog-update.html'
+  
+  def get_success_url(self):
+    pk = self.kwargs['pk'] 
+    return reverse_lazy('blog:detail', kwargs = {'pk':pk})
+
+class BlogDeleteView( DeleteView ):
+  model = Post
+  template_name = 'blog-delete.html'
+  success_url   = reverse_lazy('blog:home') 
--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'blog'
+    'blog',
+    'tailwind', 
+    'theme'
+]
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 MIDDLEWARE = [
